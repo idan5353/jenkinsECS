913bca88b660a430914c076ac16e671077137ffe
idan <idanu55@gmail.com>
2025-08-16 22:51:40 +0300

    update

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Jenkins + ECS!!! idan king");
+  res.send("🚀 Hello from Jenkins + ECS!!! check");
 });
 
 const port = process.env.PORT || 3000;
