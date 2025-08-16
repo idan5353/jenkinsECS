ed50f01e6d5e6b3ede63377f548db5c6559b9d25
idan <idanu55@gmail.com>
2025-08-16 18:33:08 +0300

    update

--- ecs.tf
+++ ecs.tf
@@ -30,18 +30,16 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  container_definitions = jsonencode([
-    {
-      name      = "my-app"
-      image     = "${aws_ecr_repository.app.repository_url}:latest"
-      essential = true
-      portMappings = [{
-        containerPort = 3000
-        hostPort      = 3000
-        protocol      = "tcp"
-      }]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "my-app"
+    image     = "${aws_ecr_repository.app.repository_url}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+      protocol      = "tcp"
+    }]
+  }])
 }
 
 resource "aws_ecs_service" "app" {
@@ -50,9 +48,15 @@ resource "aws_ecs_service" "app" {
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+  
   network_configuration {
-    subnets         = module.vpc.private_subnets
+    # ✅ Use public subnets so Fargate task can reach ECR
+    subnets         = module.vpc.public_subnets
     assign_public_ip = true
     security_groups = [aws_security_group.jenkins_sg.id]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_exec_policy
+  ]
 }
